292048eedb4a158b85f65f18087de1a8e4e38c1b
alisahebdad <ali.sahebdad@gmail.com>
2022-03-04 15:50:15 +0330

    model renew fixed

--- 2.py
+++ 2.py
@@ -335,89 +335,92 @@ def main():
     criterion = None
     
     val_loader = None
-    if args.dataset == 'cifar10':
-        model_name = '{}_{}.pth'.format(args.arch,args.dataset)
-        model_addr = os.path.join(args.model,model_name)
-        model =  model_zoo.vgg.VGG('VGG19')#torch.load(model_addr)
-        
-        __ = torch.load(model_addr)['net']
-        ___ = {}
-        for i in __:
-            ___[i[7:]] = __[i]
-
-
-
-        model.load_state_dict(___)
-        model.eval()
-        criterion = nn.CrossEntropyLoss()
-
-        print ('model load ...')
-
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-
-        transform_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-
-        trainset = torchvision.datasets.CIFAR10(
-            root=args.data, train=True, download=True, transform=transform_train)
-        val_loader = torch.utils.data.DataLoader(
-            trainset, batch_size=128, shuffle=True, num_workers=2)
-
-        testset = torchvision.datasets.CIFAR10(
-            root=args.data, train=False, download=True, transform=transform_test)
-        testloader = torch.utils.data.DataLoader(
-            testset, batch_size=100, shuffle=False, num_workers=2)
-
-        classes = ('plane', 'car', 'bird', 'cat', 'deer',
-                'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-
-
-
-    if args.dataset == 'imagenet':
-        model = models.__dict__[args.arch](pretrained=True)
-        criterion = nn.CrossEntropyLoss()
-        print ('model load')
-        crop_size = 224
-        scale = 0.875
-        large_crop_size = int(round(crop_size / scale))
-        valdir = args.data
-        val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(large_crop_size),
-            transforms.CenterCrop(crop_size),
-            transforms.ToTensor(),
-        ])),
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=8,
-        pin_memory=True)
-        testloader = val_loader
-    if torch.cuda.is_available():
-        model.cuda()
-
-    #original_model = copy.deepcopy(model)
- 
     
-    #model = simple_quant_mode_acts(model,8)
-    #model = simple_quant_mode_acts(model,8)
-    #print (simple_quant_mode_acts(model,8))
-    print ('-'*100)
-    #changeMode(model,'Forward')
-    #print (model)
-    #load_max_min_act(model,'./stat')
-    #model_org = copy.deepcopy(model)
-        
-
     for i in range(args.repeat):
+        
+        
+        if args.dataset == 'cifar10':
+            model_name = '{}_{}.pth'.format(args.arch,args.dataset)
+            model_addr = os.path.join(args.model,model_name)
+            model =  model_zoo.vgg.VGG('VGG19')#torch.load(model_addr)
+            
+            __ = torch.load(model_addr)['net']
+            ___ = {}
+            for i in __:
+                ___[i[7:]] = __[i]
+
+
+
+            model.load_state_dict(___)
+            model.eval()
+            criterion = nn.CrossEntropyLoss()
+
+            print ('model load ...')
+
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+
+            transform_test = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+
+            trainset = torchvision.datasets.CIFAR10(
+                root=args.data, train=True, download=True, transform=transform_train)
+            val_loader = torch.utils.data.DataLoader(
+                trainset, batch_size=128, shuffle=True, num_workers=2)
+
+            testset = torchvision.datasets.CIFAR10(
+                root=args.data, train=False, download=True, transform=transform_test)
+            testloader = torch.utils.data.DataLoader(
+                testset, batch_size=100, shuffle=False, num_workers=2)
+
+            classes = ('plane', 'car', 'bird', 'cat', 'deer',
+                    'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+
+
+
+        if args.dataset == 'imagenet':
+            model = models.__dict__[args.arch](pretrained=True)
+            criterion = nn.CrossEntropyLoss()
+            print ('model load')
+            crop_size = 224
+            scale = 0.875
+            large_crop_size = int(round(crop_size / scale))
+            valdir = args.data
+            val_loader = torch.utils.data.DataLoader(
+            datasets.ImageFolder(valdir, transforms.Compose([
+                transforms.Resize(large_crop_size),
+                transforms.CenterCrop(crop_size),
+                transforms.ToTensor(),
+            ])),
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=8,
+            pin_memory=True)
+            testloader = val_loader
+        if torch.cuda.is_available():
+            model.cuda()
+
+        #original_model = copy.deepcopy(model)
+    
+        
+        #model = simple_quant_mode_acts(model,8)
+        #model = simple_quant_mode_acts(model,8)
+        #print (simple_quant_mode_acts(model,8))
+        print ('-'*100)
+        #changeMode(model,'Forward')
+        #print (model)
+        #load_max_min_act(model,'./stat')
+        #model_org = copy.deepcopy(model)
+            
+
         print('-'*100,i)
         #model = copy.deepcopy(original_model)
         if args.quantize_weight != 0:
